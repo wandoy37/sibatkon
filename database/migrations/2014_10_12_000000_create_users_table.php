98178786c6a5415096363ff5e6c77f65a0948d99
wandoy37 <riswandi035@gmail.com>
2024-06-17 16:31:08 +0800

    fortify login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nama');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'petugas', 'penguji']);
             $table->rememberToken();
             $table->timestamps();
         });
