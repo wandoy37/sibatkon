ea35e160eba523c9f369b51316fc6b68f11a839f
wandoy37 <riswandi035@gmail.com>
2024-06-18 00:36:12 +0800

    fix relasi bahan dan formulir

--- app/Models/Formulir.php
+++ app/Models/Formulir.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Formulir extends Model
 {
@@ -11,4 +12,9 @@ class Formulir extends Model
 
     protected $table = 'formulirs';
     protected $guarded = [];
+
+    public function bahan(): BelongsTo
+    {
+        return $this->belongsTo(Bahan::class);
+    }
 }
