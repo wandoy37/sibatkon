fb80f77d246f12d6c40a3a5aeecb99c3ee38a3b1
wandoy37 <riswandi035@gmail.com>
2024-07-14 03:03:55 +0800

    dashboard  view fix

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -10,18 +10,6 @@
                     <i class="flaticon-home"></i>
                 </a>
             </li>
-            <li class="separator">
-                <i class="flaticon-right-arrow"></i>
-            </li>
-            <li class="nav-item">
-                <a href="#">Pages</a>
-            </li>
-            <li class="separator">
-                <i class="flaticon-right-arrow"></i>
-            </li>
-            <li class="nav-item">
-                <a href="#">Starter Page</a>
-            </li>
         </ul>
     </div>
 
